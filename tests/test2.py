939b0712588ee51fed3a519b3661fcf79c272385
ningan <ningan@douban.com>
2014-02-13 12:23:04 +0800

    add test2 for test case with temporary node

--- tests/test2.py
+++ tests/test2.py
@@ -9,16 +9,17 @@ import unittest
 from douban.beansdb import BeansDBProxy, MCStore
 
 
-class Test1(TestBeanseyeBase):
+class Test2(TestBeanseyeBase):
 
     proxy_addr = 'localhost:7905'
     backend1_addr = 'localhost:57901'
     backend2_addr = 'localhost:57902'
     backend3_addr = 'localhost:57903'
-    data_base_path = os.path.join("/tmp", "beanseye_test")
-    accesslog = os.path.join(data_base_path, 'beansproxy_test1.log')
-    errorlog = os.path.join(data_base_path, 'beansproxy_error_test1.log')
+    backend4_addr = 'localhost:57904'
 
+    data_base_path = os.path.join("/tmp", "beanseye_test")
+    accesslog = os.path.join(data_base_path, 'beansproxy_test2.log')
+    errorlog = os.path.join(data_base_path, 'beansproxy_error_test2.log')
 
     def setUp(self):
         self._init_dir()
@@ -26,18 +27,22 @@ class Test1(TestBeanseyeBase):
             os.remove(self.accesslog)
         if os.path.exists(self.errorlog):
             os.remove(self.errorlog)
+
         self.backend1 = BeansdbInstance(self.data_base_path, 57901)
         self.backend2 = BeansdbInstance(self.data_base_path, 57902)
         self.backend3 = BeansdbInstance(self.data_base_path, 57903)
+        self.backend4 = BeansdbInstance(self.data_base_path, 57904)
         self.backend1.start()
         self.backend2.start()
         self.backend3.start()
+        self.backend4.start()
         # start 3 backend, no additional temporary node
         proxy_conf = {
                 'servers': [
                     self.backend1_addr + ' F E D C B A 9 8 7 6 5 4 3 2 1 0',
                     self.backend2_addr + ' F E D C B A 9 8 7 6 5 4 3 2 1 0',
                     self.backend3_addr + ' F E D C B A 9 8 7 6 5 4 3 2 1 0',
+                    self.backend4_addr + ' -F -E -D -C -B -A -9 -8 -7 -6 -5 -4 -3 -2 -1 -0',
                 ],
                 'port': 7905,
                 'webport': 7908,
@@ -64,6 +69,7 @@ class Test1(TestBeanseyeBase):
         self._assert_data(self.backend1_addr, 'key1', data1)
         self._assert_data(self.backend2_addr, 'key1', data1)
         self._assert_data(self.backend3_addr, 'key1', data1)
+        self._assert_data(self.backend4_addr, 'key1', None, "temporary node should not have the key when all primary nodes is good")
 
         print "down backend2, proxy.get should be ok"
         self.backend2.stop()
@@ -75,16 +81,8 @@ class Test1(TestBeanseyeBase):
         with self.assertRaises(Exception) as exc:
             MCStore(self.backend2_addr).get('key2')
         self._assert_data(self.backend3_addr, 'key2', data2)
-
-        print "down backend1, proxy.get/set should fail"
-        self.backend1.stop()
-        self.assertEqual(proxy.get('key1'), data1)
-        with self.assertRaises(Exception) as exc:
-            MCStore(self.backend1_addr).get('key2')
-            MCStore(self.backend2_addr).get('key2')
-        with self.assertRaises(Exception) as exc:
-            proxy.set('key2', data2)
-
+        #"temporary node should have the key when primary nodes < 3"
+        self._assert_data(self.backend4_addr, 'key2', data2)
 
 
 
@@ -93,11 +91,15 @@ class Test1(TestBeanseyeBase):
         self.backend1.stop()
         self.backend2.stop()
         self.backend3.stop()
+        self.backend4.stop()
         self.backend1.clean()
         self.backend2.clean()
         self.backend3.clean()
+        self.backend4.clean()
         
 if __name__ == '__main__':
     unittest.main()
+
+
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
